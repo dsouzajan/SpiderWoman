b32ea819eadbf462def165ab0a82990149c42b4c
dsouzajan <18jjd3@queensu.ca>
2022-04-08 22:10:02 -0400

    r Please enter the commit message for your changes. Lines starting

--- WebCrawler.py
+++ WebCrawler.py
@@ -6,17 +6,19 @@ import concurrent.futures
 
 MAX_THREADS = 30
 crawled = []
+errors = []
 
 def search_links(urlvalue):
-    crawled.append(urlvalue)
     return_links = []
-    r = requests.get(urlvalue)
+    r = requests.get(urlvalue, headers={"User-Agent": "TestUser"})
 
     soup = BeautifulSoup(r.content, "lxml")
 
     if r.status_code != 200:
+        errors.append(urlvalue)
         return "Error"
     else:
+        crawled.append(urlvalue)
         for linkvalue in soup.findAll('a', attrs={'href': re.compile("^http")}):
             if linkvalue.get('href') not in crawled :
                 return_links.append(linkvalue.get('href'))
@@ -30,7 +32,8 @@ def web_crawl(link):
     threads = min(MAX_THREADS, len(link))
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         executor.map(search_links, return_urls)
-    print(str(len(crawled)) + " websites crawled without errors.")
+    print(str(len(crawled)) + " website(s) crawled without errors.")
+    print(str(len(errors)) + " website(s) with errors.")
     
 web_crawl(sys.argv[1])
     
